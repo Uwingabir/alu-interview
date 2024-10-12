62fae5552518436af19e9bacb446510c8fa2edf7
Uwingabir <u.caline@alustudent.com>
2024-10-12 08:11:56 -0700

    error handling

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,13 +1,22 @@
 #!/usr/bin/python3
-"""Pascal's Triangle
+"""
+Module: 0-pascal_triangle
 """
 
 
 def pascal_triangle(n):
-    """func returns a list of lists of integers representing the Pascal’s triangle
+    """
+    Generate Pascal's triangle of n levels.
+
+    Args:
+        n (int): Number of levels in Pascal's triangle.
+
+    Returns:
+        List[List[int]]: Pascal's triangle as a list of lists of integers.
     """
     if n <= 0:
         return []
+
     triangle = [[1]]
     for i in range(1, n):
         row = [1]
@@ -15,4 +24,5 @@ def pascal_triangle(n):
             row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
         row.append(1)
         triangle.append(row)
+
     return triangle
